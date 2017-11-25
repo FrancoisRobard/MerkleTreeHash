2c569dd5fc33a1105087c097009260a970882520
FrancoisRobard <francois2.robard@orange.fr>
2017-11-25 13:58:31 +0100

    Fin implémentation du Merkle Tree (reste à faire l'aglo permettant sa construction à partir d'un document)

--- src/collaborativedoctp4/InternalNode.java
+++ src/collaborativedoctp4/InternalNode.java
@@ -1,9 +1,29 @@
 package collaborativedoctp4;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class InternalNode extends Tree{
     
-    public InternalNode(MerkleTree leftTree, MerkleTree rightTree){
-        
+    public InternalNode(MerkleTree leftTree, MerkleTree rightTree) throws IOException, NoSuchAlgorithmException{
+        // The right tree is supposed to be the higher one, in term of indexes of events
+        beginningIndex = leftTree.getBegenningIndex();
+        endingIndex = rightTree.getEndingIndex();
+        leftSubTree = leftTree;
+        rightSubTree = rightTree;
+        // Getting the bytes for 0x00 and for the event
+        byte [] one = new byte[]{0x01};
+        // Concatenating the two bytes arrays
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
+        outputStream.write(one);
+        outputStream.write(leftTree.getHash());
+        outputStream.write(rightTree.getHash());
+        byte [] toBeHashed = outputStream.toByteArray( );
+        // Hashing the bytes
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        hash = digest.digest(toBeHashed);
     }
     
 }
